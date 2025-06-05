aca2c60976ec5a3008505a59bdbd15b184d3de3f
wnlen <544241974@qq.com>
2025-06-05 09:05:03 +0800

    prePage, goPath, goTab，导航文件提交

---
+++ utils/nav.js
@@ -0,0 +1,21 @@
+// 获取上一个页面实例
+export function prePage() {
+	const pages = getCurrentPages();
+	const pre = pages[pages.length - 2];
+	return typeof pre.$vm !== 'undefined' ? pre.$vm : pre;
+}
+
+// 路由跳转（判断是否登录）
+export function goPath(path, vm) {
+	uni.setStorageSync("1003", "0");
+	if (vm?.vuex_user?.phone) {
+		uni.navigateTo({ url: path });
+	} else {
+		uni.navigateTo({ url: "/pages/subUser/login" });
+	}
+}
+
+// Tab 切换
+export function goTab(path) {
+	uni.switchTab({ url: path });
+}
