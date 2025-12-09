cb61cccba038fad1eed15dae210038123715c19c
wnlen <62139570+wnlen@users.noreply.github.com>
2025-12-09 16:36:39 +0800

    Update global.js

--- common/plugins/global.js
+++ common/plugins/global.js
@@ -1,20 +1,11 @@
 // common/plugins/global.js
 import {
-	prePage,
-	goPath,
-	goTab
-} from '@/utils/nav.js';
-import {
 	nav
 } from '@/utils/nav.ts'
 
 export default {
 	install(app) {
-		app.config.globalProperties.$prePage = prePage;
-		app.config.globalProperties.$goPath = goPath;
-		app.config.globalProperties.$goTab = goTab;
-
-		// 挂载新版导航系统
+		// 挂载导航系统
 		uni.$nav = nav
 		//兼容组件中this.$nav
 		app.config.globalProperties.$nav = nav
